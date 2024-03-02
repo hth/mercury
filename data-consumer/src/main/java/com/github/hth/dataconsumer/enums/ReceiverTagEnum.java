069bd3a287e77eac1bc68eac622e9fd1c93eea9e
hth <hth@users.noreply.github.com>
2024-03-02 15:50:07 -0600

    Optimized import

--- data-consumer/src/main/java/com/github/hth/dataconsumer/enums/ReceiverTagEnum.java
+++ data-consumer/src/main/java/com/github/hth/dataconsumer/enums/ReceiverTagEnum.java
@@ -3,5 +3,5 @@ package com.github.hth.dataconsumer.enums;
 public enum ReceiverTagEnum {
     OWN,
     KAFKA,
-    CSV;
+    CSV
 }
--- data-consumer/src/main/java/com/github/hth/dataconsumer/service/ThirdPartyTransactionReceiverRunner.java
+++ data-consumer/src/main/java/com/github/hth/dataconsumer/service/ThirdPartyTransactionReceiverRunner.java
@@ -1,7 +1,6 @@
 package com.github.hth.dataconsumer.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import org.springframework.stereotype.Service;
