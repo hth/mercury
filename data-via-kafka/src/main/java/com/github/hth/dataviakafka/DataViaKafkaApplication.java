9453e8c847d45852211123ff1533cae4c2d41954
hth <hth@users.noreply.github.com>
2024-03-02 07:31:42 -0600

    Initial data via kafka

---
+++ data-via-kafka/src/main/java/com/github/hth/dataviakafka/DataViaKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.hth.dataviakafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataViaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataViaKafkaApplication.class, args);
+	}
+
+}
---
+++ data-via-kafka/src/test/java/com/github/hth/dataviakafka/DataViaKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hth.dataviakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataViaKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
