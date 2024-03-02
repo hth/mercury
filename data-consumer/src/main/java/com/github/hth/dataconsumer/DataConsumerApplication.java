b246daf03065c63d33e0425a84abc6c6ce9775c8
hth <hth@users.noreply.github.com>
2024-03-01 23:50:11 -0600

    Initial - Blank

---
+++ data-consumer/src/main/java/com/github/hth/dataconsumer/DataConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.hth.dataconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataConsumerApplication.class, args);
+    }
+
+}
---
+++ data-consumer/src/test/java/com/github/hth/dataconsumer/DataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hth.dataconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
