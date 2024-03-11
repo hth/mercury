fc62778a18a8cbd89e4c43492c9716f995795c0b
hth <hth@users.noreply.github.com>
2024-03-11 13:24:26 -0500

    Initial CSV

---
+++ data-csv/src/main/java/com/github/hth/datacsv/DataCsvApplication.java
@@ -0,0 +1,13 @@
+package com.github.hth.datacsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCsvApplication.class, args);
+	}
+
+}
