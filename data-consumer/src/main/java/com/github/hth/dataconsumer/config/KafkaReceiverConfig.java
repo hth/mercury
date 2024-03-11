0bc84ebec203083ea257fae799a2f6c1850c4fff
hth <hth@users.noreply.github.com>
2024-03-11 08:01:22 -0500

    Changed to use dynamic group instance id

--- data-consumer/src/main/java/com/github/hth/dataconsumer/config/KafkaReceiverConfig.java
+++ data-consumer/src/main/java/com/github/hth/dataconsumer/config/KafkaReceiverConfig.java
@@ -1,6 +1,8 @@
 package com.github.hth.dataconsumer.config;
 
 import com.github.hth.dataconsumer.dto.CreditTransactionDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +11,10 @@ import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
 import reactor.kafka.receiver.ReceiverOptions;
 
 import java.util.List;
+import java.util.UUID;
 
 @Configuration
+@Slf4j
 public class KafkaReceiverConfig {
 
     @Value("${external.transaction.topic}")
@@ -18,6 +22,8 @@ public class KafkaReceiverConfig {
 
     @Bean
     public ReceiverOptions<String, CreditTransactionDTO> receiverOptions(KafkaProperties kafkaProperties) {
+        kafkaProperties.getProperties().put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, UUID.randomUUID().toString());
+        log.info("Consumer Properties {}", kafkaProperties.getProperties());
         return ReceiverOptions.<String, CreditTransactionDTO>create(kafkaProperties.buildConsumerProperties())
             .subscription(List.of(externalTransactionTopic));
     }
